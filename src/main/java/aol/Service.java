64606c70eb607db3ba8189dae26c589a35b994ee
Guzman, David <david.guzman@teamaol.com>
2016-08-15 11:35:59 +0100

    Hello devfest on path /

--- src/main/java/aol/Service.java
+++ src/main/java/aol/Service.java
@@ -4,10 +4,10 @@ import javax.ws.rs.*;
 import com.aol.micro.server.auto.discovery.Rest;
 
 @Rest
-@Path("/status")
+@Path("/")
 public class Service {
     @GET
     public String ping() {
-        return "ok";
+        return "Hello world";
     }
 }
